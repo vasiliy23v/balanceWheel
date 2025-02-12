162d7e4b7dfe474e2ae28c0615078880bd76906a
Vasiliy Sh <shefvasiliys2003@gmail.com>
2025-02-12 17:29:15 +0200

    feature: - Add vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import BalanceWheel from './App.jsx'
 import './i18n.js'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Analytics />
     <BalanceWheel />
   </StrictMode>,
 )
